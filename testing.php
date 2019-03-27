7703247d64bc02c619b4e0fe4017213dba0a9ed5
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-03-27 22:22:26 +0100

    collection get

--- testing.php
+++ testing.php
@@ -11,3 +11,22 @@ $client = new Client('http://localhost:8000', array(
     )
 ));
 
+$nickname = 'ObjectOrienter'.rand(0, 999);
+$data = array(
+    'nickname' => $nickname,
+    'avatarNumber' => 5,
+    'tagLine' => 'A test dev!'
+);
+
+$request = $client->post('/api/programmers', null, json_encode($data));
+$response = $request->send();
+
+$url = $response->getHeader('Location');
+$request = $client->get($url);
+$response = $request->send();
+
+$request = $client->get('/api/programmers');
+$response = $request->send();
+
+echo $response;
+echo "\n\n";
